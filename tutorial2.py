9ebf12d37c05a5275cdd4348826f969111c638ae
willl4 <williamlaycock@hanovernorwichschools.org>
2017-02-03 14:44:54 -0500

    Update tutorial2.py

--- tutorial2.py
+++ tutorial2.py
@@ -7,10 +7,13 @@ blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 thinline = LineStyle(1, black)
 mycircle = CircleAsset(5, thinline, blue)
-xcoordinates = range(100, 600, 10)
+mycircle2 = CircleAsset(5, thinline, red)
+x1coordinates= range(100, 300, 20)
+xcoordinates = range(400, 500, 10)
 
 # Generate a list of sprites that form a line!
 sprites = [Sprite(mycircle, (x, x*0.5 + 100)) for x in xcoordinates]
+sprites = [Sprite(mycircle2, (x, x*0.5 + 200)) for x in x1coordinates]
 
 myapp = App()
 myapp.run()
