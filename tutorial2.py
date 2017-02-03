ed127bd637ef7d96b38d281b8dd91ea067227d29
willl4 <williamlaycock@hanovernorwichschools.org>
2017-02-03 14:36:40 -0500

    Update tutorial2.py

--- tutorial2.py
+++ tutorial2.py
@@ -1,1 +1,16 @@
+from ggame import App, Color, LineStyle, Sprite
+from ggame import CircleAsset
 
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+thinline = LineStyle(1, black)
+mycircle = CircleAsset(5, thinline, blue)
+xcoordinates = range(100, 600, 10)
+
+# Generate a list of sprites that form a line!
+sprites = [Sprite(mycircle, (x, x*0.5 + 100)) for x in xcoordinates]
+
+myapp = App()
+myapp.run()
